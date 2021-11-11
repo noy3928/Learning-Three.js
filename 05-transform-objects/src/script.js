15c51fdf6f20e7d745cf22a72bb5e4e116c289ff
yechan <noy3928@naver.com>
2021-11-11 23:49:44 +0900

    transform을 위한 각종 메서드들

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -18,6 +18,29 @@ mesh.position.y = -0.6
 mesh.position.z = 1
 scene.add(mesh)
 
+//position의 메서드들.
+// console.log(mesh.position.normalize())
+// mesh.position.set(0.6, -0.3, 3)
+
+
+// Scale 
+mesh.scale.x = 2
+mesh.scale.y = 0.5
+mesh.scale.z = 0.5
+//여기서도 set을 사용할 수 있음. 
+
+
+//rotation
+mesh.rotation.y = 6.5
+mesh.rotation.x = 0.5
+
+
+
+//Axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper)
+
+
 /**
  * Sizes
  */
@@ -32,6 +55,9 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+//카메라와 obj와의 거리값.
+console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
