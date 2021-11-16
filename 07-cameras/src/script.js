3e38981cf856ba6f149f10433c6461c4a42bee87
yechan <noy3928@naver.com>
2021-11-16 22:36:41 +0900

    orbitControls

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
 
 //cursor /마우스의 위치를 얻는 방법 
 const cursor = {
@@ -7,11 +8,11 @@ const cursor = {
     y:0
 }
 
-window.addEventListener('mousemove', (event) => 
-{
-    cursor.x = event.clientX / sizes.width - 0.5 // 화면 위에서의 마우스 위치. -0.5를 한 것은, 왼쪽과 오른쪽의 값을 구하기 위한 것이다. 
-    cursor.y = -(event.clientY / sizes.height - 0.5)
-})
+// window.addEventListener('mousemove', (event) => 
+// {
+//     cursor.x = event.clientX / sizes.width - 0.5 // 화면 위에서의 마우스 위치. -0.5를 한 것은, 왼쪽과 오른쪽의 값을 구하기 위한 것이다. 
+//     cursor.y = -(event.clientY / sizes.height - 0.5)
+// })
 
 
 /**
@@ -42,12 +43,17 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1,4) 
 // const aspectRatio = sizes.width / sizes.height
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100) // 화면으로부터 계산해서 뭔가를 그린다. 그래서 화면의 비율에 따라서 모양이 달라진다. 
 
-// camera.position.x = 2
+camera.position.x = 2
 // camera.position.y = 2
 camera.position.z = 2
 console.log(camera.position.length())
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+//Controls
+const controls = new OrbitControls(camera, canvas) // 두개의 인자를 가진다. 카메라와 dom엘리먼트
+controls.target.y = 1
+controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -65,6 +71,7 @@ const tick = () =>
     //update camera
     camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
     camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    camera.position.y = cursor.y * 5
     // camera.lookAt(new THREE.Vector3())
     camera.lookAt(mesh.position)
 
