e9257babad4a9ce203db09f97d2612d5a36e65fb
yechan <noy3928@naver.com>
2021-11-19 07:38:51 +0900

    dbclick event

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -41,6 +41,14 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 })
 
+window.addEventListener('dblclick', () => {
+if(!document.fullscreenElement){
+    console.log('go fullscreen')
+}else{
+    console.log('leave fullscreen')
+}
+})
+
 /**
  * Camera
  */
